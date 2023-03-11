713fd266a7b6e3e3d942b80d591e8ffc2f3de774
lalitwebhopers <lalit.webhopers@gmail.com>
2023-03-11 16:25:33 +0530

    Route Variables

--- src/Routes/Web.php
+++ src/Routes/Web.php
@@ -43,16 +43,27 @@ class Web
             $route = str_replace('}', '@', $route);
             $route_array = explode('@', $route);
             if (count($route_array) % 2 == 0) return false;
-            $variables = [];
             $regex_route = $route;
             for ($i = 1; ($i <= (count($route_array) - 1) / 2); $i++) {
-                $variables[] = $route_array[$i * 2 - 1];
                 $regex_route = str_replace('@' . $route_array[$i * 2 - 1] . '@', '.*?', $regex_route);
             }
 
-            // 
+            $params = [];
             $regex_route = str_replace('/', '\/', $regex_route);
-            if (preg_match('/(^' . $regex_route . '$)/', $main_url)) return true;
+            if (preg_match('/(^' . $regex_route . '$)/', $main_url)) {
+                $temp_route = $route;
+                $temp_url = $main_url;
+                while (strlen($temp_route)) {
+                    $a = explode('@', $temp_route);
+                    $temp_url = preg_replace('/' . preg_quote($a[0], '/') . '/', '', $temp_url, 1);
+                    $b = explode('/', $temp_url, 2);
+                    $a = explode('@', $a[1]);
+                    $temp_route = $a[1];
+                    $params[$a[0]] = $b[0];
+                }
+                // var_dump($params);
+                return true;
+            }
         }
 
         // exact match
